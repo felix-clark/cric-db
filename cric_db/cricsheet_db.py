f59766653953da1d957472e6395b2ed2fdf9c146
Felix Clark <mfclark3690@gmail.com>
2021-12-12 13:21:18 -0500

    small refactoring

--- cric_db/cricsheet_db.py
+++ cric_db/cricsheet_db.py
@@ -2,6 +2,52 @@ from argparse import ArgumentParser
 import sqlite3
 import os
 import json
+from typing import List
+
+
+def get_player_fields() -> List[str]:
+    player_fields = [
+        'hash TEXT PRIMARY KEY',
+        'name TEXT',
+        # NOTE: In theory some players could play on multiple teams
+        'team TEXT',
+    ]
+    return player_fields
+
+
+def get_match_fields() -> List[str]:
+    match_fields = [
+            'id INT PRIMARY KEY',
+            'balls_per_over INT DEFAULT 6',
+            'city TEXT',
+            # Maybe just keep start/end dates?
+            # Some SQL implementations have a DATE type but not sqlite (?)
+            'dates BLOB',
+            # TODO: maintain separate event database?
+            'event_match_number INT',
+            'event_name TEXT',
+            'gender TEXT',
+            'match_type TEXT',
+            'match_type_number INT',
+            # ...
+            'season TEXT',
+            'team_type TEXT',
+            # teams should always be a list of 2 teams
+            'team_a TEXT',
+            'team_b TEXT',
+            # .. many more
+        ]
+    return match_fields
+
+
+def get_schema(table_name: str, fields: List[str]) -> str:
+    fields_lines = ',\n'.join(fields)
+    schema = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+        {fields_lines}
+        )
+        """
+    return schema
 
 
 def build_from_cricsheet():
@@ -14,33 +60,7 @@ def build_from_cricsheet():
     db_con = sqlite3.connect(args.db)
     cur = db_con.cursor()
 
-    match_fields = [
-        'id INT PRIMARY KEY',
-        'balls_per_over INT DEFAULT 6',
-        'city TEXT',
-        # Maybe just keep start/end dates?
-        # Some SQL implementations have a DATE type but not sqlite (?)
-        'dates BLOB',
-        # TODO: maintain separate event database?
-        'event_match_number INT',
-        'event_name TEXT',
-        'gender TEXT',
-        'match_type TEXT',
-        'match_type_number INT',
-        # ...
-        'season TEXT',
-        'team_type TEXT',
-        # teams should always be a list of 2 teams
-        'team_a TEXT',
-        'team_b TEXT',
-        # .. many more
-    ]
-    match_fields_lines = ',\n'.join(match_fields)
-    match_schema = f"""
-        CREATE TABLE IF NOT EXISTS matches (
-        {match_fields_lines}
-        )
-        """
+    match_schema = get_schema("matches", get_match_fields())
     print(match_schema)
     # Could executemany or executescript for greater efficiency
     cur.execute(match_schema)
@@ -54,7 +74,7 @@ def build_from_cricsheet():
         "team_b",
     )
     input_jsons = args.input_jsons
-    for match_json in input_jsons[:4]:
+    for match_json in input_jsons[:10]:
         match_data = {}
         match_json_base = os.path.basename(match_json)
         assert match_json_base[-5:] == ".json"
@@ -65,8 +85,15 @@ def build_from_cricsheet():
         # keys are ['meta', 'info', 'innings']
         # print(match_dict.keys())
         match_meta = match_dict["meta"]
+        match_data["data_version"] = match_meta["data_version"]
+        match_data["data_revision"] = match_meta["revision"]
+        match_data["data_created"] = match_meta["created"]
+
         match_info = match_dict["info"]
+        print(match_info)
+        # "innings" has the ball-by-ball data
         innings = match_dict["innings"]
+        # print(innings)
         match_data["match_type"] = match_info["match_type"]
         match_data["match_type_number"] = match_info["match_type_number"]
         # match_data["dates"] = match_info["dates"]
@@ -87,7 +114,7 @@ def build_from_cricsheet():
         # print(match_meta)
         # print(match_info)
 
-    for row in cur.execute("SELECT * FROM matches"):
+    for row in cur.execute("SELECT * FROM matches LIMIT 3"):
         print(row)
 
 
